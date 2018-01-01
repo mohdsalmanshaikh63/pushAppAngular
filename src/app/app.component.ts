cbbb9b05b83cefe03562b745852c4b6f5e4da9b6
DonAjit <ajitshikalgar786@gmail.com>
2018-01-01 20:45:09 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -8,10 +6,53 @@ declare var $:any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+   title: String;
+  names: any;
+  selectedAll: any;
+  singleSelected : any;
+  buttonIndicator : any;
+   constructor() {
+    this.title = "Select all/Deselect all checkbox - Angular 2";
+	this.buttonStatus = false;
+	
+	//Fetch data and set users into names array
+    this.names = [
+      { name: 'Prashobh', selected: false },
+      { name: 'Abraham', selected: false },
+      { name: 'Anil', selected: false },
+      { name: 'Sam', selected: false },
+      { name: 'Natasha', selected: false },
+      { name: 'Marry', selected: false },
+      { name: 'Zian', selected: false },
+      { name: 'karan', selected: false },
+    ]
+	
+    
+}
 
-  constructor() {}
-
+selectAll() {
+    for (var i = 0; i < this.names.length; i++) {
+      this.names[i].selected = this.selectedAll;
+    }
+  }
+  checkIfAllSelected() {
+    this.selectedAll = this.names.every(function(item:any) {0
+        return item.selected == true;
+      })
+  }
+  
+  singleSelected(){
+	  for (var i = 0; i < this.names.length; i++) 
+		if(this.names[i].selected == true){
+			return true;
+		}
+  return false;
+  }
+  toggleButton(){
+  if(!(this.selectedAll == true))
+  return buutonStatus = true;
+  }
+  
   ngOnInit() {
     $(document).foundation();
   }
